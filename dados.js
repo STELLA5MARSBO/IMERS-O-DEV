3128c444625c013693cb9f79a7888acf1abb02fb
Stella Borges <stellaborgestec@outlook.com>
2024-09-04 22:17:52 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados =[
+    {
+        titulo: "Ada Lovelace",
+        descricao: "Considerada a primeira programadora de computadores, Ada Lovelace trabalhou com Charles Babbage no desenvolvimento da Máquina Analítica, um precursor dos computadores modernos. Seus algoritmos e ideias sobre a capacidade dos computadores de processar informações além de números foram fundamentais para o desenvolvimento da computação.",
+        link: "https://en.wikipedia.org/wiki/Ada_Lovelace"
+    },
+    {
+        titulo: "Marie Curie",
+        descricao: "Pioneira na pesquisa sobre radioatividade, Marie Curie foi a primeira mulher a ganhar um Prêmio Nobel, e a única a ganhar em duas categorias diferentes (Física e Química). Suas descobertas revolucionaram a medicina e a física nuclear, abrindo caminho para novas áreas de pesquisa e tratamento de doenças.",
+        link: "https://www.nobelprize.org/prizes/physics/1903/curie/biographical/"
+    },
+    {
+        titulo: "Rosalind Franklin",
+        descricao: "A biofísica Rosalind Franklin produziu imagens de difração de raios X do DNA que foram cruciais para a descoberta da estrutura de dupla hélice da molécula. Seus trabalhos foram fundamentais para a compreensão da genética e dos processos biológicos.",
+        link: "https://www.nature.com/articles/537292a"
+    }
+]
